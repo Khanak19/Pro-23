d1d282130133e0e6e0eae2250247a0275db9575e
Khanak19 <85015299+Khanak19@users.noreply.github.com>
2021-06-15 17:39:48 +0530

    Add files via upload

--- PlayerBase.js
+++ PlayerBase.js
@@ -3,7 +3,7 @@ class PlayerBase {
       var options = {
         isStatic: true
       };
-      this.image = loadImage("lib/assets/base1.png");
+      this.image = loadImage("/assets/base1.png");
       this.width = width;
       this.height = height;
       this.body = Bodies.rectangle(x, y, this.width, this.height, options);
